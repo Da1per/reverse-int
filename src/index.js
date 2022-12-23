197ea0a7bcb832f03db013f279a83102a129e984
Da1per <kiborg7075@gmail.com>
2022-12-23 11:08:38 +0300

    feat:add code in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if(n.toString()[0]=='-'){
+        let revers= n.toString().split('').reverse();
+        revers.pop();  
+        return revers.join('')
+    }
+    else{
+        return n.toString().split('').reverse().join(''); 
+    }
 }
+
